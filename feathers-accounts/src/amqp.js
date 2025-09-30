74e4ac5046930be4164cb5d551ce640819f5a258
Dávid Ondruš <hi@davidondrus.sk>
2025-09-30 20:43:27 +0200

    Add deadletter

--- feathers-accounts/src/amqp.js
+++ feathers-accounts/src/amqp.js
@@ -3,22 +3,37 @@ import onOrderCreated from './events/order.created.js';
 
 export const amqp = async app => {
   const amqpConnection = await amqplib.connect('amqp://rabbitmq:rabbitmq@rabbitmq');
+
   const amqpListenerChannel = await amqpConnection.createChannel();
-  await amqpListenerChannel.assertQueue('feathers-accounts');
+  await amqpListenerChannel.assertExchange('deadletter', 'topic');
+  await amqpListenerChannel.assertQueue('deadletters');
+  await amqpListenerChannel.bindQueue('deadletters', 'deadletter', '#');
+
+  await amqpListenerChannel.assertQueue('feathers-accounts', {
+    deadLetterExchange: 'deadletter',
+  });
   await amqpListenerChannel.bindQueue('feathers-accounts', 'amq.topic', 'order.#');
+
   await amqpListenerChannel.consume('feathers-accounts', async message => {
     const {routingKey} = message.fields;
     const messageBody = JSON.parse(message.content.toString());
 
-    switch (routingKey) {
-      case 'order.created': {
-        await onOrderCreated(app, amqpListenerChannel, messageBody, message);
-        break;
+    try {
+      switch (routingKey) {
+        case 'order.created': {
+          // Simulate error for deadletter
+          throw new Error('Error');
+          await onOrderCreated(app, amqpListenerChannel, messageBody, message);
+          break;
+        }
+        default: {
+          amqpListenerChannel.ack(message);
+          break;
+        }
       }
-      default: {
-        amqpListenerChannel.ack(message);
-        break;
-      }
+    } catch (error) {
+      console.error('Deadlettering message');
+      amqpListenerChannel.nack(message, false, false);
     }
   });
 }
